071fda8ebc54aba11a2fddd3d685404c8b564016
maehundey <144396460+maehundey@users.noreply.github.com>
2024-10-26 23:00:32 -0400

    Update ObjectTabs.tsx

--- homepage/src/components/ObjectTabs.tsx
+++ homepage/src/components/ObjectTabs.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import TimeTimeline, { DisplayEvents } from './TimeTimeline';
 
 const objectData = [
   { name: 'Object 1' },
@@ -9,14 +10,23 @@ const objectData = [
 
 export default function ObjectTabs() {
   return (
-    <div className="overflow-auto" style={{ width: '2199px', height: '30%' }}>
+    <div>
       <div className="flex bg-gray-200 p-2 gap-2">
+        <div className='mt-5'>
+          <TimeTimeline />  
+        </div>
         {objectData.map((item, index) => (
-          <div
-            key={index}
-            className="bg-gray-300 w-32 h-8 rounded-md flex items-center justify-center text-black"
-          >
-            {item.name}
+          <div>
+            <div
+              key={index}
+              className="bg-gray-300 w-32 h-8 rounded-md flex items-center justify-center text-black"
+            >
+              {item.name}
+            </div>
+            <div className='flex'>
+
+              <DisplayEvents/>
+            </div>
           </div>
         ))}
       </div>
